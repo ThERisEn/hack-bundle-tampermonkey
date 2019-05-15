67452f0bdb32c1e2e4a11f322df50f667b2e7202
TEAMOVERSONIC <49201819+TEAMOVERSONIC@users.noreply.github.com>
2019-05-15 14:06:08 -0600

    Update arras hack.js

--- scripts/arras hack.js
+++ scripts/arras hack.js
@@ -1,1 +1,30 @@
 
+// ==UserScript==
+// @name         arras.io hack
+// @namespace    arras.io
+// @version      1.0.0
+// @description  arras hack in dev
+// @author       unknown
+// @match        *://arras.io/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+main();
+   
+})();
+function main(){
+initUi();
+  function initUi(){
+    var playername = document.getElementById("PlayerNameInput").value();
+    var isInjected = false;
+    if(!isInjected){
+    alert("ARRAS INJECTED");
+     isInjected = true;
+    }
+    function adblock(){
+document.getElementById("arras-io_336x280").remove();
+}
+adblock();
+  }
+}
